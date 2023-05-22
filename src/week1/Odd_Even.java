29f3742a14e325648c85f5ad63ad72b9d051a3e6
merve <mervefeyzakaraca@gmail.com>
2023-05-22 21:13:17 +0200

    added changes

--- src/week1/Odd_Even.java
+++ src/week1/Odd_Even.java
@@ -1,10 +1,15 @@
 package week1;
 
 public class Odd_Even {
-/* Numbers -- odd & even
-Write a method that can identify  a given number is even or odd.
-EX:
-       identify(5) --> "Odd"
-       identify(6) --> "Even"*/
 
 }
+
+
+
+
+/* Numbers -- odd & even
+        Write a method that can identify  a given number is even or odd.
+        EX:
+        identify(5) --> "Odd"
+        identify(6) --> "Even"
+        */
