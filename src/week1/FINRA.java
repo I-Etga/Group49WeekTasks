410129e3e56d58497da2a153260417caf195744d
omeragir <omeragir08@gmail.com>
2023-05-22 19:39:58 +0200

    FINRA class created

--- src/week1/FINRA.java
+++ src/week1/FINRA.java
@@ -1,4 +1,24 @@
 package week1;
 
 public class FINRA {
+    public static void main(String[] args) {
+
+        String result = "";
+
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result = "FINRA ";
+            } else if (i % 5 == 0) {
+                result = "RA ";
+            } else if (i % 3 == 0) {
+                result = "FIN ";
+            } else {
+                result = i + " ";
+            }
+            System.out.print(result);
+        }
+
+
+    }
 }
+
