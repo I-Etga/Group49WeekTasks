81e532c7f74c357e3402b5a265ca7292a01391d2
mkyksll <mervefeyzakaraca@gmail.com>
2023-05-23 23:29:44 +0200

    added changes

--- src/week1/FINRA.java
+++ src/week1/FINRA.java
@@ -1,4 +1,21 @@
 package week1;
 
 public class FINRA {
+    public static void main(String[] args) {
+
+        String result = "";
+        for (int i = 1; i <= 30; i++) {
+            if (i % 15 == 0) {
+                result += "FINRA ";
+            } else if (i % 3 == 0) {
+                result += "FIN ";
+            } else if (i % 5 == 0) {
+                result += "RA ";
+            } else {
+                result += i + " ";
+            }
+        }
+        System.out.print(result);
+    }
+
 }
--- src/week1/Odd_Even.java
+++ src/week1/Odd_Even.java
@@ -1,6 +1,29 @@
 package week1;
 
+import javax.swing.plaf.PanelUI;
+import java.util.Scanner;
+
 public class Odd_Even {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter a number: " );//we will see on console
+
+        int number = scan.nextInt();
+
+        if( number>0 &&  number%2 ==0){
+            System.out.println("The given number is even: "+number);
+        }else{
+            System.out.println("The given number is odd: "+number);
+        }
+
+        scan.close();
+
+
+    }
+
 
 }
 
