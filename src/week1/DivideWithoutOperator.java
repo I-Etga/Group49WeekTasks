077c6ec113e726211acdbb9fab54d8358daf9f45
omeragir <omeragir08@gmail.com>
2023-05-24 08:53:42 +0200

    Divide method created

--- src/week1/DivideWithoutOperator.java
+++ src/week1/DivideWithoutOperator.java
@@ -1,4 +1,20 @@
 package week1;
 
 public class DivideWithoutOperator {
+
+    public static void divideMethod(int number1, int number2) {
+
+        int count = 0;
+        while (number1 >= number2) {
+            number1 -= number2;
+            count++;
+        }
+        System.out.println(count + " with a remainder of " + number1);
+
+
+    }
+
+
+
+
 }
