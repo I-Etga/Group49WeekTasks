3a5199ef5bf315c4097856e54e8111e9c92e55ea
SMirza <shokhidamir@gmail.com>
2023-05-24 10:24:52 +0200

    DivideWithoutOperator task added

--- src/week1/DivideWithoutOperator.java
+++ src/week1/DivideWithoutOperator.java
@@ -2,3 +2,10 @@ package week1;
 
 public class DivideWithoutOperator {
 }
+
+
+
+/*
+Numbers -- Divide without / operator
+Write a method that can divide two numbers without using division operator.
+ */
