627cc25aa7ee0a2acab62ea756f3f197495f6a43
omeragir <omeragir08@gmail.com>
2023-05-24 08:53:54 +0200

    Divide method created

--- src/week1/Odd_Even.java
+++ src/week1/Odd_Even.java
@@ -1,6 +1,17 @@
 package week1;
 
 public class Odd_Even {
+
+    public static String identify(int number){
+
+        if (number%2==0){
+            return "Even";
+        }else {
+            return "Odd";
+        }
+    }
+
+
 /* Numbers -- odd & even
 Write a method that can identify  a given number is even or odd.
 EX:
