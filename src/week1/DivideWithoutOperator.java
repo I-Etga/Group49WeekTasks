55a2071093992ec110731857894e88593df932b9
sevilayunlu <sevilayunlu@icloud.com>
2023-05-31 11:42:42 +0200

    added changes by Sevilay

--- src/week1/DivideWithoutOperator.java
+++ src/week1/DivideWithoutOperator.java
@@ -1,4 +1,27 @@
 package week1;
 
 public class DivideWithoutOperator {
+
+    public static int divideWithoutDivisionOperator(int num1, int num2) {
+        if (num1 == 0) {
+            throw new ArithmeticException("The result is undefined. The number cannot be divided by 0");
+        }
+
+        int count = 0;
+        while (num2 >= num1) {
+            num2 -= num1;
+            count++;
+        }
+        return count;
+    }
+
+
+
+
+
 }
+/*
+2-Numbers -- Divide without / operator
+Write a method that can divide two numbers without using division operator.
+
+ */
--- src/week1/Odd_Even.java
+++ src/week1/Odd_Even.java
@@ -1,10 +1,34 @@
 package week1;
 
 public class Odd_Even {
+
+    public static String identifyOddOrEven(int number) {
+
+        String result = "";
+
+        if (number != 0) {
+
+            if (number % 2 == 0) {
+                result = "Even";
+            } else {
+                result = "Odd";
+            }
+        } else {
+            result = "Undefined";
+        }
+
+        return result;
+    }
+
+}
+//
+
+
+
+
+
 /* Numbers -- odd & even
 Write a method that can identify  a given number is even or odd.
 EX:
        identify(5) --> "Odd"
        identify(6) --> "Even"*/
-
-}
