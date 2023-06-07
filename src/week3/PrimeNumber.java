9de731df2885b7f6cdbb427d24ebd74d8f3aa395
omeragir <omeragir08@gmail.com>
2023-06-07 19:41:33 +0200

    Week3

--- src/week3/PrimeNumber.java
+++ src/week3/PrimeNumber.java
@@ -1,8 +1,36 @@
 package week3;
 
 public class PrimeNumber {
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
 
+        if (number == 2) {
+            return true;
+        }
+
+        // Check for divisibility from 2 to the square of the number
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false; // Number is divisible, not prime
+            }
+        }
+
+        return true; // Number is prime
+    }
+
+    public static void main(String[] args) {
+        int num = 17;
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
 }
+
+
 /*
 1-Numbers -- Prime Number
 Write a method that can check if a number is prime or not.
--- src/week3/ReverseNegativeNumber.java
+++ src/week3/ReverseNegativeNumber.java
@@ -1,7 +1,35 @@
 package week3;
 
-public class ReverseNegativeNumber {
+import java.util.Scanner;
 
+public class ReverseNegativeNumber {
+    public static int reverseNegativeNumber(int num) {
+        boolean isNegative = false;
+
+        if (num < 0) {
+            isNegative = true;
+            num *= -1;  // Make the number positive for reversal
+        }
+
+        int reversedNum = 0;
+        while (num > 0) {
+            reversedNum = (reversedNum * 10) + (num % 10);
+            num /= 10;
+        }
+
+        if (isNegative) {
+            reversedNum *= -1;  // Make the number negative again
+        }
+
+        return reversedNum;
+    }
+
+    public static void main(String[] args) {
+        int input = -35;
+        int reversed = reverseNegativeNumber(input);
+        System.out.println("Input: " + input);
+        System.out.println("Reversed: " + reversed);
+    }
 }
 /*
 2-Numbers -- Reverse negative number
