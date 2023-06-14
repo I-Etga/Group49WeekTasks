e3907601d11b091c4f49953cefc4dbc27b9d1cbe
i.etga <akayetga@gmail.com>
2023-06-14 11:11:15 +0200

    Week3

--- src/week3/PrimeNumber.java
+++ src/week3/PrimeNumber.java
@@ -1,6 +1,30 @@
 package week3;
 
 public class PrimeNumber {
+    public static void primeOrNot(int number) {
+        if (number <= 1) {
+            System.out.println(number + " is not prime.");
+            return;
+        }
+
+        // Check for divisibility from 2 to the square root of the number
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                System.out.println(number + " is not prime.");
+                return; // Number is divisible, so not prime
+            }
+        }
+
+        System.out.println(number + " is prime.");
+    }
+
+    public static void main(String[] args) {
+        int number = 37;
+        primeOrNot(number);//37 is prime.
+
+        number = 8;
+        primeOrNot(number);//8 is not prime.
+    }
 
 }
 /*
--- src/week3/ReverseNegativeNumber.java
+++ src/week3/ReverseNegativeNumber.java
@@ -2,6 +2,18 @@ package week3;
 
 public class ReverseNegativeNumber {
 
+    public static int reverseNegativeNumber(int number){
+        String num = Integer.toString(number).replace("-","");
+
+        String reverseNumber ="";
+        for (int i = num.length()-1; i >= 0; i--) {
+            reverseNumber += num.charAt(i);
+        }
+        return Integer.parseInt(reverseNumber) * (-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(reverseNegativeNumber(-35));
+    }
 }
 /*
 2-Numbers -- Reverse negative number
