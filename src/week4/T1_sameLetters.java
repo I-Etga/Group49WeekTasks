3172ccc2a77f8bd5b9333c5805ba5f72cc0308bf
i.etga <akayetga@gmail.com>
2023-06-14 11:11:19 +0200

    Week4

--- src/week4/T1_sameLetters.java
+++ src/week4/T1_sameLetters.java
@@ -1,7 +1,34 @@
 package week4;
 
-public class T1_sameLetters {
+import java.util.Arrays;
 
+public class T1_sameLetters {
+    public static boolean checkSameLetters(String str1, String str2) {
+        // Convert both strings to lowercase for case-insensitive comparison
+        String lowerStr1 = str1.toLowerCase();
+        String lowerStr2 = str2.toLowerCase();
+
+        // Convert the strings to char arrays and sort them
+        char[] charArray1 = lowerStr1.toCharArray();
+        char[] charArray2 = lowerStr2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        // Compare the sorted char arrays
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = "cab";
+        boolean result = checkSameLetters(str1, str2);
+        System.out.println(result); // Output: true
+
+        String str3 = "abc";
+        String str4 = "abb";
+        result = checkSameLetters(str3, str4);
+        System.out.println(result); // Output: false
+    }
 }
 /*
 String -- Same letters
