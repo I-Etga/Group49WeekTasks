349bd4ae7091f96b03cc8f18f24fd617e025ac30
i.etga <akayetga@gmail.com>
2023-06-21 10:05:40 +0200

    Week5 done

--- src/week5/FindTheUnique.java
+++ src/week5/FindTheUnique.java
@@ -1,7 +1,29 @@
 package week5;
 
 public class FindTheUnique {
+    public static String findUniqueCharacters(String input) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            boolean isUnique = true;
+            for (int j = 0; j < input.length(); j++) {
+                if (i != j && currentChar == input.charAt(j)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                result.append(currentChar);
+            }
+        }
+        return result.toString();
+    }
 
+    public static void main(String[] args) {
+        String input = "AAABBBCCCDEF";
+        String uniqueCharacters = findUniqueCharacters(input);
+        System.out.println(uniqueCharacters);
+    }
 }
 /*
 1-String - Find the unique
--- src/week5/Reverse.java
+++ src/week5/Reverse.java
@@ -1,7 +1,24 @@
 package week5;
 
+import javax.print.DocFlavor;
+
 public class Reverse {
 
+    public static String reverse(String str) {
+
+        String reversedStr = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedStr += str.charAt(i);
+        }
+
+        return reversedStr;
+    }
+
+    public static void main(String[] args) {
+        String str = "ABCD";
+        System.out.println(reverse(str));
+
+    }
 }
 /*
 2-String -- Reverse
