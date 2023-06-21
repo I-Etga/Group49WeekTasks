9df983c5b91be5ac6197fac70656f0a5566e8029
i.etga <akayetga@gmail.com>
2023-06-21 12:06:16 +0200

    week5 tasks done

--- src/week5/FindTheUnique.java
+++ src/week5/FindTheUnique.java
@@ -1,7 +1,28 @@
 package week5;
 
-public class FindTheUnique {
+import java.util.Arrays;
+import java.util.Collections;
 
+public class FindTheUnique {
+    public static String uniqueChar(String str) {
+
+        String result = "";
+        for (String each : str.split("")) {
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each);
+            if (frequency == 1) {
+                result += each;
+            } else {
+                continue;
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String str = "AAABBBCCCDEF";
+        System.out.println(uniqueChar(str));
+    }
 }
 /*
 1-String - Find the unique
