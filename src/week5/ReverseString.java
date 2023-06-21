c4dae9064c9df1bd6ca376f4ce10437a9f9f6860
Ismailduman <issmailduman@gmail.com>
2023-06-21 11:30:57 +0200

    Reverse String

---
+++ src/week5/ReverseString.java
@@ -0,0 +1,21 @@
+package week5;
+
+public class ReverseString {
+
+        public static String reverse(String str) {
+            StringBuilder reversed = new StringBuilder();
+
+            for (int i = str.length() - 1; i >= 0; i--) {
+                reversed.append(str.charAt(i));
+            }
+
+            return reversed.toString();
+        }
+
+        public static void main(String[] args) {
+            String original = "ABCD";
+            String reversed = reverse(original);
+            System.out.println(reversed);
+        }
+    }
+
