48cbd9ecf97337ead5907358d1e61b58db0b6bd3
mamihami <hamide.pehlivan@hotmail.com>
2023-06-21 10:11:14 +0200

    week 4 tasks created

---
+++ src/week4/T1_sameLetters.java
@@ -0,0 +1,12 @@
+package week4;
+
+import java.util.Arrays;
+
+public class T1_sameLetters {
+/*
+String -- Same letters
+Write a return method that check if a string is build out of the same letters as another string.
+Ex: same("abc", "cab"); -> true
+same("abc", "abb"); -> false:
+ */
+}
---
+++ src/week4/T2_removeDublicates.java
@@ -0,0 +1,12 @@
+package week4;
+
+public class T2_removeDublicates {
+
+}
+/*
+String -- Remove Duplicates
+Write a return method that can remove the duplicated values from String
+Ex: removeDup("AAABBBCCC") ==> ABC
+ */
+
+
