1f8f8d55d39c0f84228034b09671fab4416d82c9
HKCigdem <c.emirzaesmer@gmail.com>
2023-06-21 11:35:18 +0200

    week5

--- src/week5/FindTheUnique.java
+++ src/week5/FindTheUnique.java
@@ -2,9 +2,37 @@ package week5;
 
 public class FindTheUnique {
 
+
+    public static String findUniqueCharacters(String input) {
+
+        StringBuilder uniqueChars = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+
+            if (input.indexOf(currentChar) == input.lastIndexOf(currentChar)) {
+                uniqueChars.append(currentChar);
+            }
+        }
+
+        return uniqueChars.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String input = "AAABBBCCCDEF";
+        String uniqueChars = findUniqueCharacters(input);
+
+        System.out.println(uniqueChars);  // Output: DEF
+
+    }
+
 }
+
+
 /*
 1-String - Find the unique
  Write a return method that can find the unique characters from the String
  Ex: unique("AAABBBCCCDEF") ==> "DEF";
  */
+
--- src/week5/Reverse.java
+++ src/week5/Reverse.java
@@ -2,7 +2,30 @@ package week5;
 
 public class Reverse {
 
+    public static String reverseString(String input) {
+
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+
+        return reversed.toString();
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        String input = "ABCD";
+        String reversedString = reverseString(input);
+
+        System.out.println(reversedString);  // Output: DCBA
+    }
+
 }
+
 /*
 2-String -- Reverse
  Write a function that can reverse a String
