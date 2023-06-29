e63f4ddd60ad96626e8cc90dd39ab1dca86b9e86
i.etga <akayetga@gmail.com>
2023-06-29 14:19:46 +0200

    week6 tasks done

--- src/week6/FindMaximum.java
+++ src/week6/FindMaximum.java
@@ -2,6 +2,22 @@ package week6;
 
 public class FindMaximum {
 
+    public static int maxNumber(int[] numbers) {
+        int max = numbers[0];
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (max < numbers[i]) {
+                max = numbers[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {99, 12, 23, 32, 44, 57, 6};
+        System.out.println(maxNumber(arr));
+    }
 }
 /*
 Array_FindMaximum
