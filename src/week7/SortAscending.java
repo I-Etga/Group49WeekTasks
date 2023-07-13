0d3155dd49e9cdb21f86b799a523bc7b7939cd32
i.etga <akayetga@gmail.com>
2023-07-13 19:08:42 +0200

    Week7 tasks done

--- src/week7/SortAscending.java
+++ src/week7/SortAscending.java
@@ -1,6 +1,33 @@
 package week7;
 
+import java.util.Arrays;
+
 public class SortAscending {
+    public static int[] sortArrayAscending(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("Array cannot be null or empty.");
+        }
+
+        // Perform bubble sort algorithm
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap array[j] and array[j+1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 9, 8, 7};
+        int[] sortedNumbers = sortArrayAscending(arr);
+        System.out.println("Sorted array: " + Arrays.toString(sortedNumbers));
+    }
 
 }
 /*
--- src/week7/SortDescending.java
+++ src/week7/SortDescending.java
@@ -1,7 +1,34 @@
 package week7;
 
-public class SortDescending {
+import java.util.Arrays;
 
+public class SortDescending {
+    public static int[] sortArrayDescending(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("Array cannot be null or empty.");
+        }
+
+        // Perform bubble sort algorithm
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    // Swap array[j] and array[j+1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,20,7, 8, 90};
+        int[] sortedNumbers = sortArrayDescending(arr);
+        System.out.println("Sorted array: " + Arrays.toString(sortedNumbers));
+    }
 }
 /*
 Question3: Array - Sort Descending
