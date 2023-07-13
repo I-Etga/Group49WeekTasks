df5569a79542844603d12b977d4182bca5794f50
sevilayunlu <sevilayunlu@icloud.com>
2023-07-13 18:47:24 +0200

    added week 7 array min num

--- src/week7/FindMinimum.java
+++ src/week7/FindMinimum.java
@@ -1,6 +1,27 @@
 package week7;
 
 public class FindMinimum {
+    public static int  minNum(int[] numbers){
+        int min=numbers[0];//assume that the first element is the minimum number
+
+        for (int i = 1; i < numbers.length; i++) {//i=1,2,3,4...
+
+            if(numbers[i]<min){//compares the element of the array with the current min number
+                min=numbers[i];//replace the current min number
+            }
+
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers={5,4,7,9,1,2};
+        System.out.println("minNum(numbers) = " + minNum(numbers));
+
+    }
+
 
 }
 /*
